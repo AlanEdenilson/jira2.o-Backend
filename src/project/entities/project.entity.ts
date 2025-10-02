8a7bc62e89f13fbe2674f79b1fbe26b36fcc2bf5
Alan Edenilson Campos Sanchez <cs25001@esfe.agape.edu.sv>
2025-10-02 16:22:38 -0600

    creacion de la entidad de projecto

---
+++ src/project/entities/project.entity.ts
@@ -0,0 +1,44 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export enum ProjectStatus {
+  ACTIVO = 'activo',
+  COMPLETADO = 'completado',
+  CANCELADO = 'cancelado',
+}
+
+@Entity('projects')
+export class Project {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  description: string;
+
+  @Column({
+    type: 'enum',
+    enum: ProjectStatus,
+    default: ProjectStatus.ACTIVO,
+  })
+  status: ProjectStatus;
+
+  @Column({ type: 'varchar', length: 10 })
+  color: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
